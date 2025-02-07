33e7beddf722e57c0ea0e569c60a96f5a8e79466
SamsAcolyte <182761871+SamsAcolyte@users.noreply.github.com>
2025-02-07 14:46:04 +0300

    create function getComputerChoice

--- code.js
+++ code.js
@@ -2,3 +2,25 @@ const playButton = document.querySelector('button')
 const computerScore = document.querySelector('.compS')
 const humanScore = document.querySelector('.humS')
 const log = document.querySelector('.log')
+
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random()*3);
+    
+
+    switch (randomNumber) {
+        case 0 :
+            return "rock";
+            
+        
+        case 1:
+            return "paper";
+           
+
+        case 2 :
+            return "scissors";
+               
+    }
+    
+    
+
+}
