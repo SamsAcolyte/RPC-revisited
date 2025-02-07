93faef2f62aebc2d4f603d4dd6c1866946936636
SamsAcolyte <182761871+SamsAcolyte@users.noreply.github.com>
2025-02-07 13:36:52 +0300

    link html elements to js

--- code.js
+++ code.js
@@ -1,1 +1,4 @@
-const playButton = document.querySelector(button)
+const playButton = document.querySelector('button')
+const computerScore = document.querySelector('.compS')
+const humanScore = document.querySelector('.humS')
+const log = document.querySelector('.log')
