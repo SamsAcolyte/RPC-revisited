53eacef70d7ab0242bcba81da04c059d0002c2b8
SamsAcolyte <182761871+SamsAcolyte@users.noreply.github.com>
2025-02-07 15:05:20 +0300

    decalre humanChoice and add event listener to select for it

--- code.js
+++ code.js
@@ -2,6 +2,13 @@ const playButton = document.querySelector('button')
 const computerScore = document.querySelector('.compS')
 const humanScore = document.querySelector('.humS')
 const log = document.querySelector('.log')
+const select = document.querySelector ("select")
+
+let humanChoice;
+
+select.addEventListener('change', function () {
+humanChoice = select.value
+})
 
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random()*3);
@@ -22,3 +29,6 @@ function getComputerChoice () {
 
 }
 
+function getHumanChoice () {
+
+}
